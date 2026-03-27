8c7b4592f01abdec3124b66b585dae7bc033389d
Laura Lara <larala7789Qgmail.com>
2026-03-27 07:29:17 -0500

    feat: Impelement routes in Postman for users

--- REST API/src/app.js
+++ REST API/src/app.js
@@ -1,7 +1,9 @@
 
 import express from "express";
+import userRoutes from "./models/routes/user.routes.js";
 
 const app = express();
 app.use(express.json())
+app.use(userRoutes);
 
 export default app
---
+++ REST API/src/controller/users.controller.js
@@ -0,0 +1,32 @@
+import { User } from "../models/User.js";
+
+export const getUsers = async (req, res) => {
+    const users = await User.findAll();
+    return res.json(users);
+};
+
+//cargar objeto de usuario al request body para crear un nuevo usuario
+export const createUser = async (req, res) => {
+    const newUser = await User.create(req.body);
+    return res.json(newUser);
+};
+
+
+export const updateUser = async (req, res) => {
+    const id  = req.params.id;
+    const user = await User.findByPk(id);
+    try {
+        await user.update(req.body);
+    } catch (error) {
+        console.log
+    }
+    return res.json(user);
+};
+
+
+export const deleteUser = async (req, res) => {
+    const id  = req.params.id;
+    const user = await User.findByPk(id);
+    await user.destroy();
+    return res.sendStatus(204); // delete ok
+};
